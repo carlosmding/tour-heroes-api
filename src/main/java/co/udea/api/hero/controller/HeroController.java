9354bf74968f50af22b06153e60251bc73090b9c
carlosmding <carlos.pinto@udea.edu.co>
2023-03-28 19:31:53 -0500

    Inicialización de proyecto con avances realizados en clase.

---
+++ src/main/java/co/udea/api/hero/controller/HeroController.java
@@ -0,0 +1,38 @@
+package co.udea.api.hero.controller;
+
+import co.udea.api.hero.model.Hero;
+import co.udea.api.hero.service.HeroService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/heroes")
+public class HeroController {
+
+    private final Logger log = LoggerFactory.getLogger(HeroController.class);
+
+    private HeroService heroService;
+
+    public HeroController(HeroService heroService){
+        this.heroService = heroService;
+    }
+
+    @GetMapping("{id}")
+    @ApiOperation(value = "Busca un hero por su id",  response = Hero.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Hero encontrado existosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<Hero> getHero(@PathVariable Integer id){
+        log.info("Rest request buscar heroe por id: "+ id);
+        return ResponseEntity.ok(heroService.getHero(id));
+    }
+}
